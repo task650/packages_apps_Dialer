06103f1bf6e4ab38c35e3d313c53f7e36c1e7c91
Danesh M <daneshm90@gmail.com>
2014-07-02 20:57:34 -0700

    Dialer : Don't hold on to views used in listviews
    Lets not hold references to views used inside of a listview adapter since it
    will try to recycle it and if something is holding a reference to that view it is
    trying to recycle, it will block things on the ui thread and queue up all events during that time
    and only dispatch once focus is lost.
    
    Change-Id: Idd113e4f13f08d706a33fd4d07fd9a12f5382671

--- src/com/android/dialer/DialtactsActivity.java
+++ src/com/android/dialer/DialtactsActivity.java
@@ -1022,6 +1022,10 @@ public class DialtactsActivity extends TransactionSafeActivity implements View.O
         return tm.getCallState() != TelephonyManager.CALL_STATE_IDLE;
     }
 
+    public void allContactsClick(View v) {
+        onShowAllContacts();
+    }
+
     @Override
     public void onShowAllContacts() {
         final Intent intent = new Intent(this, AllContactsActivity.class);
